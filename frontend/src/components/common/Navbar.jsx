a6ff45175c6e5d9e6ab173f5394a84e355d81f2f
Yogesh Bhambhu <yogeshbhambhu81@gmail.com>
2026-07-24 15:01:38 +0530

    nav bar

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -228,7 +228,7 @@ function Navbar({ onMobileMenuToggle }) {
               onClick={() => { setAccountOpen(o => !o); setNotifOpen(false); }}
             >
               <IoPersonCircleOutline />
-              <span className="navbar-avatar-label">Sunny Singh</span>
+              <span className="navbar-avatar-label"></span>
             </button>
 
             {accountOpen && (
@@ -238,7 +238,7 @@ function Navbar({ onMobileMenuToggle }) {
                     <IoPersonCircleOutline size={42} />
                   </div>
                   <div>
-                    <p className="account-name">Sunny Singh</p>
+                    <p className="account-name">Yogesh Bhambhu</p>
                     <p className="account-email">sunny1524.be23@chitkarauniversity.edu.in</p>
                   </div>
                 </div>
